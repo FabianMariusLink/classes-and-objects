7eff2e0e77eb1ce9f0ff2e699890c222f88a3a80
Dominic Bergmann <dominic.bergmann@gmail.com>
2023-09-26 11:57:58 +0200

    Schritt 5: Erstellt ein zweites Objekt der Klasse 'Auto', beschleunige es und gebt die Geschwindigkeit aus.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Car dominicsCar = new Car("VW", "Van", "white", 2010, 200, true);
+        Car dominicsCar = new Car("VW", "Van", "white", 2010, 0, true);
+        Car enricosCar = new Car("Trabant", "onlyOne", "grey", 1989, 0, false);
+
         dominicsCar.startCar();
         dominicsCar.increaseCarSpeed(250);
+
+        enricosCar.startCar();
+        enricosCar.increaseCarSpeed(100);
     }
 }
