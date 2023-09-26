5c55c2bf96a2ca551fb2fa33542e8d58b362746e
Dominic Bergmann <dominic.bergmann@gmail.com>
2023-09-26 12:15:03 +0200

    Bonusaufgabe mit Personen implementiert

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,13 +2,27 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Car dominicsCar = new Car("VW", "Van", "white", 2010, 0, true);
-        Car enricosCar = new Car("Trabant", "onlyOne", "grey", 1989, 0, false);
+        Car dominicsCar = new Car("VW", "Van", "white", 2010, 200, true);
+        Car enricosCar = new Car("Trabant", "onlyOne", "grey", 1989, 100, false);
 
         dominicsCar.startCar();
-        dominicsCar.increaseCarSpeed(250);
+        dominicsCar.increaseCarSpeed("VW",250);
+        System.out.println(dominicsCar.speed);
 
         enricosCar.startCar();
-        enricosCar.increaseCarSpeed(100);
+        enricosCar.increaseCarSpeed("Trabant",100);
+        System.out.println(enricosCar.speed);
+
+
+        Person person1 = new Person("Max", 25, "männlich");
+        Person person2 = new Person("Anna", 30, "weiblich");
+        Person person3 = new Person("Chris", 22, "männlich");
+
+        // Ruft die Vorstellungsmethode auf
+        person1.introduce();
+        person2.introduce();
+        person3.introduce();
+
+
     }
 }
---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Person {
+    public String name;
+    public int age;
+    public String gender;
+
+    public Person(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public void introduce() {
+        System.out.println("Hallo, ich bin " + name + " und " + age + " Jahre alt.");
+    }
+}
